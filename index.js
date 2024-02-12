df1d516d2d9670e24282291b3be4d27d272b71b3
Collin Bullock <collin@collinbullock.com>
2024-02-12 15:19:32 -0600

    got the data working at the desired route

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const importData = require("./data.json");
 let port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.send("You're working hard, buddy")
+})
+
+app.get("/apiData", (req, res) => {
+  res.send (importData)
 })
 
 app.listen(port, () => {
