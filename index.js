dea17d7e24a7d6c9857739d13de0fe2338e4512f
Collin Bullock <collin@collinbullock.com>
2024-02-12 15:20:51 -0600

    added a note for myself

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// I stopped the video (https://www.youtube.com/watch?v=u2Df5A4BS8s) at 5:10
+
 const express = require("express");
 const app = express();
 const importData = require("./data.json");
